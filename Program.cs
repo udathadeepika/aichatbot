8301776234ea91f5b88441dbf9f4cf109093f3ca
udathadeepika <166320119+udathadeepika@users.noreply.github.com>
2025-10-17 19:50:56 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hey Iam ur ai chatbot, Welcome! ");
+        Console.WriteLine("Enter 'exit' to close ur response");
+        Dictionary<string, string> response = new Dictionary<string, string>()
+        {
+            { "hi", "Hello welcome, how can I help you?" },
+    { "namaste", "Hey, are you Telugu?" },
+    { "how are you", "I’m doing great, thanks for asking!" },
+    { "your name", "I’m an AI chatbot created in C#." },
+    { "bye", "Goodbye! Have a nice day!" }
+
+        };
+        while (true)
+        {
+            Console.Write("You: ");
+            string UserInput = Console.ReadLine().ToLower();
+
+            if (UserInput == "exit")
+                break;
+            bool replied = false;
+            foreach( var pair in response)
+            {
+                if (UserInput.Contains(pair.Key))
+                {
+                    Console.WriteLine("Bot: "+ pair.Value);
+                    replied = true;
+                    break;
+                }
+                
+            }
+            if (!replied)
+            {
+                Console.WriteLine("bot:  sorryyy i did not understand");
+            }
+
+        }
+    }
+}
